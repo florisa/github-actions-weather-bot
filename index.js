6acf919b6ad832a8706d2c67644f01ed4a7a8e4a
florisa <florisa.martins@rwth-aachen.de>
2021-06-10 16:54:18 +0200

    change units

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const weatherToken = process.env.WEATHER_API_TOKEN
 const weatherURL = new URL('https://api.openweathermap.org/data/2.5/weather')
 weatherURL.searchParams.set('zip', '52070,de')
 weatherURL.searchParams.set('APPID', weatherToken)
-weatherURL.searchParams.set('units', 'imperial')
+weatherURL.searchParams.set('units', 'metric')
 
 
 const getWeatherData = async () => {
